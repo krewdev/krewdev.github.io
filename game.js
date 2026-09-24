7b9658dfc7c7ad6ba93e325200c20e18387eb14d
James Young <jcandjcandav@gmail.com>
2026-09-23 20:23:32 -0400

    Add THE BLOCK engine (game.js)

---
+++ game.js
@@ -0,0 +1,8 @@
+const CITIES = [
+  {id:0, name:"BROOKLYN",    plant:0.55, kit:1.10, street:0.80, cert:0.70, heat:0.6, cop:0.28, blurb:"Greenhouses. Cheap GREEN. Cops are bored until you scale."},
+  {id:1, name:"QUEENS",      plant:0.90, kit:0.60, street:0.85, cert:0.75, heat:0.8, cop:0.35, blurb:"Kit vendors under the 7. The lab smell is just metadata."},
+  {id:2, name:"MANHATTAN",   plant:1.30, kit:1.25, street:1.45, cert:1.55, heat:1.3, cop:0.62, blurb:"Everyone has a ticker. Everyone has a lawyer. Bids are real."},
+  {id:3, name:"THE BRONX",   plant:0.85, kit:0.95, street:1.20, cert:1.10, heat:1.15,cop:0.50, blurb:"Thin books. Fat spreads. Don’t flash unlocked cash."},
+  {id:4, name:"CONEY ISLAND",plant:1.00, kit:0.90, street:1.35, cert:1.40, heat:1.2, cop:0.48, blurb:"The Pit. Curves print in public. Ads are cheap. Dignity isn’t."},
+  {id:5, name:"BRIDGEPORT",  plant:1.15, kit:1.20, street:1.10, cert:1.25, heat:0.9, cop:0.40, blurb:"The docks. If it graduates, it can leave. If it’s hot, it burns."}
+];
